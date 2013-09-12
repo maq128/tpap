05eec037dbb583959133bc691fc5d97cd8f0d8c6
maq128 <maq128@126.com>
2013-09-12 18:26:51 +0800

    -

---
+++ testcase/common/caja_foot.php
@@ -0,0 +1,50 @@
+﻿</div><!-- J_TScriptedModule -->
+
+<!--这里是将自己的js让服务端编译一下，配置下服务端的php路径和自己的js即可，注意路径-->
+<?php
+// 对指定的 js 文件进行 caja compile，保存编译结果，并返回保存的文件名
+function cajaCompile( $origJsFile )
+{
+	$cajaJsFile = $origJsFile . '.cajaed.js';
+	$origJsContent = file_get_contents( $origJsFile );
+
+	$data = array(
+		'token=TAE-SDK',
+		'content=' . urlencode( $origJsContent ),
+		'component=uniqueSign'
+	);
+	$data = implode( '&', $data );
+
+    $curl = curl_init( 'http://zxn.taobao.com/tbcajaService.htm' );
+    curl_setopt( $curl, CURLOPT_SSL_VERIFYHOST, 1 );
+    curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, false );
+    curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
+    curl_setopt( $curl, CURLOPT_POST, 1 );
+    curl_setopt( $curl, CURLOPT_POSTFIELDS, $data );
+    $res = curl_exec( $curl );
+
+    if ( ! curl_errno( $curl ) ) {
+		$res = substr( $res, strpos( $res, 'TShop' ) );
+		$res = str_replace( '&#39;', "'", $res );
+		$res = str_replace( '&quot;','"', $res );
+		$res = str_replace( '&amp;', '&', $res );
+		$res = str_replace( "\\\\\"", "\\\"", $res );
+        file_put_contents( $cajaJsFile, $res );
+    }
+    curl_close( $curl );
+    return $cajaJsFile;
+}
+
+if ( isset($_GET['native']) ) {
+	echo '<script src="' . $baseName . '.js"></script>';
+} else {
+	echo '<script type="text/javascript" src="' . cajaCompile( $baseName . '.js' ) . '"></script>';
+	echo '<script src="../../assets/base/setup.js"></script>';
+}
+?>
+</body>
+</html>
+<?php
+$html = ob_get_contents();
+file_put_contents( $baseName . '.html', $html );
+ob_end_flush();
---
+++ testcase/common/caja_head.php
@@ -0,0 +1,25 @@
+﻿<?php
+$baseName = basename( strtolower($_SERVER['SCRIPT_NAME']), '.php' );
+ob_start();
+?><!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+<script type="text/javascript" src="http://a.tbcdn.cn/s/kissy/1.3.0/kissy-min.js"></script>
+<script type="text/javascript" src="http://a.tbcdn.cn/apps/taesite/balcony/core/r3002/caja/caja-min.js"></script>
+
+<script type="text/javascript" src="../../assets/base/caja-util.js"></script>
+<script type="text/javascript" src="../../assets/base/caja-log.js"></script>
+<script type="text/javascript" src="../../assets/base/balcony.js"></script>
+<script type="text/javascript" src="../../assets/openjs/kissy/1.3.0/adaptor.js"></script>
+<script src="../../test/assets.js"></script>
+</head>
+<body>
+
+<!--
+    需要测试的dom结构，注意，最外层<div class="J_TScriptedModule" data-componentid="uniqueSign"> 的class和为属性不可修改
+    用户的javascript理论上只可以作用到这个dom下面，不可以"越界"
+-->
+
+<div id="dom-test" data-componentid="uniqueSign" class="J_TScriptedModule">
